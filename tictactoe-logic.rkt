89efda024ef72e2161d9af6fb94ac2d39457cf10
Eduardo <eduardo.bm10@estudiantec.cr>
2022-05-05 00:16:57 -0600

    Update logic

--- tictactoe-logic.rkt
+++ tictactoe-logic.rkt
@@ -1,15 +1,29 @@
 #lang racket
+
 ;;Funcion para iniciar el juego Tic Tac Toe
+
 (define (TTT rows columns)
   (cond ((and (>= rows 3) (>= columns 3) (<= rows 10) (<= columns 10))
-        (+ 8 6))))
+        (build-matrix '() rows columns))))
 
-(define (build-matrix matrix rows columns)
-  (cond ((= (length? matrix) rows)
+;;________________________________________________________________________
+
+;; Funciones para armar una matrix rxc:
+;; build-matrix: funcion principal inserta filas dentro de una lista.
+;; build-matrix-aux: Funcion auxiliar inserta columnas dentro de una fila.
+
+(define (build-matrix matrix r c)
+  (cond ((= (length? matrix) r)
          matrix)
         ( else
-          ;;(insert-row matrix columns)
-          (build-matrix matrix rows columns))))
+          (build-matrix (cons (build-matrix-aux '() c) matrix) r c))))
+
+(define (build-matrix-aux r c)
+  (cond ((= (length? r) c)
+         r)
+        ( else
+          (build-matrix-aux (cons '() r) c))))
+;;__________________________________________________________________________
 
 (define (length? list)
   (cond ((null? list)
@@ -17,6 +31,16 @@
         (else
          (+ 1 (length? (cdr list))))))
 
+(define (value? list pos)
+  (cond ((= pos (- (length? list) (- (length? list) 1)))
+         (car list))
+        ( else
+          1)))
+
+(define (player-selection i j)
+  (cond ((= (length? '(1 2 3 4)) i)
+         (value? '(1 2 3 4) i))))
+  
 
         
          
