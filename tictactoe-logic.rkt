5f4789e1d8e83034e3f1e41fa7932d18311fa9e0
Eduardo <eduardo.bm10@estudiantec.cr>
2022-05-10 23:02:56 -0600

    Create greedy algorithm

--- tictactoe-logic.rkt
+++ tictactoe-logic.rkt
@@ -25,28 +25,80 @@
           (build-matrix-aux (cons '() r) c))))
 ;;__________________________________________________________________________
 
+;; Funciones para tratamiento de listas
+
 (define (length? list)
   (cond ((null? list)
          0)
         (else
          (+ 1 (length? (cdr list))))))
 
-(define (player-selection matrix i j)
-  (cond ((= i 1)
-         (player-selection-aux (car matrix) j))
+(define (exist? value list)
+  (cond ((null? list)
+         #f)
+        ((equal? (car list) value)
+         #t)
         (else
-          (player-selection (cdr matrix) (- i 1) j))))
+         (exist? value (cdr list)))))
+;;___________________________________________________________________________
 
-(define (player-selection-aux list counter)
-  (cond ((= counter 1)
+;; Funcion de seleccion de una casilla
+;; matrix: La matriz sobre la que se selecciona.
+;; turn: simbolo que se inserta (0 para O y 1 para X)
+;; i: fila elegida.
+;; j: columna elegida. 
+(define (selection matrix turn i j)
+  (cond ((= i 1)
+         (selection-aux (car matrix) turn j))
+        (else
+          (selection (cdr matrix) (- i 1) j))))
+
+(define (selection-aux list turn j)
+  (cond ((and (= j 1) (equal? turn 0)) 
+         (car list))
+        ((and (= j 1) (equal? turn 1))
          (car list))
         (else
-         (player-selection-aux (cdr list) (- counter 1)))))
-  
-;;(define (bot-selection matrix i j))
-  
-        
-         
-        
+         (selection-aux (cdr list) (- j 1)))))
+;;____________________________________________________________________________
 
-  
+;; Algoritmo voraz de seleccion de la maquina.
+;; matrix: La matriz sobre la que se selecciona
+(define (greedy-bot-selection matrix)
+  (selection matrix 0 (greedy-row matrix 1) (greedy-column matrix)))
+
+;; Funcion voraz para elegir la fila.
+;; val: valor de retorno (0 para retornar la fila escogida y 1 para retornar el numero de fila) 
+(define (greedy-row matrix val)
+  (greedy-row-comp matrix val 1))
+
+;; Funcion complementaria de greedy-row
+;; counter: parametro que guarda el numero de fila actual.
+(define (greedy-row-comp matrix val counter)
+  (cond ((null? matrix)
+         0)
+        ((or (exist? 0 (car matrix))(exist? 1 (car matrix)))
+         (cond ((= val 0)
+                (car matrix))
+               ((= val 1)
+                counter)))
+        (else
+         (greedy-row-comp (cdr matrix) val (+ counter 1)))))
+
+;; Funcion voraz para elegir la columna luego de elegir la fila.
+;; matrix: La matriz sobre la que se selecciona
+(define (greedy-column matrix)
+  (greedy-column-comp (greedy-row matrix 0) 1))
+
+;; Funcion complementaria de greedy-column.
+;; row: fila escogida por el algoritmo voraz.
+;; counter: parametro que guarda el numero de columna actual.
+(define (greedy-column-comp row counter)
+  (cond ((null? row)
+         0)
+        ((or (equal? (car row) 0) (equal? (car row) 1))
+         (+ counter 1))
+        ((or (equal? (cadr row) 0) (equal? (cadr row) 1))
+         counter)
+        (else
+         (greedy-column-comp (cdr row) (+ counter 1)))))
