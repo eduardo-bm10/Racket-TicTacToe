2567537366eac321b67d09bc415eac8e4a1385bd
Ashley <43053275+ashleyvasquez@users.noreply.github.com>
2022-05-11 22:35:04 -0600

    Update Interfaz.rkt

--- Interfaz.rkt
+++ Interfaz.rkt
@@ -3,7 +3,7 @@
 (require racket/format)
 
 ;---------------------------INTERFAZ GRÁFICA VENTANA 1-------------------------------
-
+;Crea la ventana de inicio
 (define frame1 (new frame%
                    [label "TicTacToe Inicio"]
                    [width 500]
@@ -12,9 +12,11 @@
 
 (new message%[parent frame1][label "Escoja el tamaño de la matriz"] )
 
+;Crea los espacios de entrada de texto
 (define col_entry (new text-field% [label "Columnas"] [parent frame1]))
 (define row_entry (new text-field% [label "Filas         "] [parent frame1]))
 
+;Llamada de acción del botón "check"
 (define (button-callback b e)
     (let ((cols (send col_entry get-value))(rows(send row_entry get-value)))
       
@@ -23,14 +25,15 @@
         [(> ( string->number rows)  10)  (new message%[parent frame1][label "La cantidad de filas debe ser máximo 10"])]
         [else (Gridcreator ( string->number rows)  ( string->number cols) 1 1)]
 
-      ) 
-      ))
-    
+      ) ))
+      
+;Crea el botón check    
 (new button% [label "Check"]
     [parent frame1]
     [callback button-callback])
 
-
+;Muestra la ventana de inicio
 (send frame1 show #t)
+
 
 
