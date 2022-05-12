60a164fd1a3b5f3b36e68effc148404d1e3c5df1
Ashley <43053275+ashleyvasquez@users.noreply.github.com>
2022-05-11 20:47:00 -0600

    Update tictactoe-logic.rkt
    ventana de inicio

--- tictactoe-logic.rkt
+++ tictactoe-logic.rkt
@@ -2,8 +2,40 @@
 (require racket/gui/base)
 (require racket/format)
 
+;---------------------------INTERFAZ GRÁFICA VENTANA 1-------------------------------
+;Crea la ventana de inicio
+(define frame1 (new frame%
+                   [label "TicTacToe Inicio"]
+                   [width 500]
+                   [height 500]))
 
-;---------------------------INTERFAZ GRÁFICA-------------------------------
+
+(new message%[parent frame1][label "Escoja el tamaño de la matriz"] )
+
+;Crea los espacios de entrada de texto
+(define col_entry (new text-field% [label "Columnas"] [parent frame1]))
+(define row_entry (new text-field% [label "Filas         "] [parent frame1]))
+
+;Llamada de acción del botón "check"
+(define (button-callback b e)
+    (let ((cols (send col_entry get-value))(rows(send row_entry get-value)))
+      
+      (cond
+        [(> ( string->number cols)  10 ) (new message% [parent frame1][label "La cantidad de columnas debe ser máximo 10"] )]
+        [(> ( string->number rows)  10)  (new message%[parent frame1][label "La cantidad de filas debe ser máximo 10"])]
+        [else (Gridcreator ( string->number rows)  ( string->number cols) 1 1)]
+
+      ) ))
+      
+;Crea el botón check    
+(new button% [label "Check"]
+    [parent frame1]
+    [callback button-callback])
+
+;Muestra la ventana de inicio
+(send frame1 show #t)
+
+;---------------------------INTERFAZ GRÁFICA VENTANA 2-------------------------------
 
 ;Creación Window
 (define frame (new frame% [label "Tarea 1"]))
@@ -43,6 +75,7 @@
 
 ;Crea el grid
 (define (Gridcreator Fila Columna Filaaux Columnaaux)
+  (send frame show #t)
   (cond [(equal?  Filaaux (+ Fila 1))];Se generaron todas las filas
         
         [(equal? Columnaaux (+ Columna 1));Se generaron todas las columnas de una fila
@@ -462,5 +495,3 @@
         (else
          (greedy-column-comp (cdr row) (+ counter 1)))))
 
-; Show the frame by calling its show method
-(send frame show #t)
